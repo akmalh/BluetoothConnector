754c9c96d9aa8a7d7bf789b2612a250fd6819ac5
Akmal Hossain <Shuvo@Akmals-MacBook-Pro.local>
2015-08-25 17:35:06 +1000

    After creating first two functions

--- BluetoothConnector/ViewController.swift
+++ BluetoothConnector/ViewController.swift
@@ -7,18 +7,48 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController , CBCentralManagerDelegate, CBPeripheralDelegate{
+    
+    var bluetoothOn: Bool = false
+    var cManager = CBCentralManager()
+    var pManager = CBPeripheralManager()
+
+    @IBOutlet weak var outputTextView: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("Test")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func centralManagerDidUpdateState(central: CBCentralManager!) {
+        
+    }
+    
+    func tLog(msg: String) {
+        self.outputTextView.text = "\r\n\r\n".stringByAppendingString(self.outputTextView.text)
+        self.outputTextView.text = msg.stringByAppendingString(self.outputTextView.text)
+    }
+    
+    @IBAction func startAction(sender: AnyObject) {
+        
+        if (!self.bluetoothOn)
+        {
+            self.tLog("Bluetooth is off")
+            return
+        }
+        
+        cManager.scanForPeripheralsWithServices(nil, options: nil)
     }
 
 
